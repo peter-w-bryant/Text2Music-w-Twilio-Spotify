1a8ccdc5825589b7c52006ed5140eb6aac3deafa
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-09-13 20:39:07 -0500

    Updated comments and requirements.txt

--- spotify.py
+++ spotify.py
@@ -1,14 +1,17 @@
 # Simple Python file for interacting with the Spotify Search API
 import requests
-
 '''
 GET_TRACK_URL: Sends a request using the requests library to the Spotify Search API, parses the JSON response,
 and returns the URL for the preview of the first track that matches the song title.
 '''
 def get_track_url(song_title, access_token):
-    spotify_url = 'https://api.spotify.com/v1/search'
-    params = {'q': song_title, 'type': 'track'}
+    spotify_url = 'https://api.spotify.com/v1/search'  # The URL endpoint for the Spotify Search API.
+    params = {'q': song_title, 'type': 'track'}        # The parameters for the request to the Spotify Search API.
+
+    # Send a request to the Spotify Search API.
     spotify_response = requests.get(spotify_url, params=params, headers={'Authorization': 'Bearer ' + access_token}).json()
-    track_url = spotify_response['tracks']['items'][0]['preview_url']
-    return track_url
+
+    track_url = spotify_response['tracks']['items'][0]['preview_url'] # Parse the JSON response to get the URL for the song preview.
+
+    return track_url  # Return the URL for the song preview.
 
